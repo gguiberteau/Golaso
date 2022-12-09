f7356e47bc84a3e37039c3b77bcf7d55b7fe3364
gguiberteau <93789571+gguiberteau@users.noreply.github.com>
2022-12-09 10:18:43 +0100

    BottomNavBar

--- app/src/main/java/es/unex/giiis/golaso/MainActivity.java
+++ app/src/main/java/es/unex/giiis/golaso/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import es.unex.giiis.golaso.databinding.ActivityMainBinding;
@@ -37,7 +38,21 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-    }
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+
+            if (destination.getId() == R.id.nav_login) {
+
+                binding.navView.setVisibility(View.GONE);
+
+            } else {
+
+                binding.navView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        }
 
     public void onRestart() {
         super.onRestart();
